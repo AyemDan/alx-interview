d07d465ad3c60a265ae16ccda9f9377985458c5c
AyemDanm <danielMMomodu@gmail.com>
2024-11-08 11:31:50 +0100

    triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,17 +1,63 @@
 #!/usr/bin/python3
 
-def factorial(num):
+"""
+This module provides functions to compute the factorial,
+combinations, and Pascal's Triangle.
+
+Functions:
+    factorial(num: int) -> int
+    Computes the factorial of a number.
+
+    combination(n: int, r: int) -> int
+    Computes the number of combinations (n choose r).
+
+    pascal_triangle(n: int) -> List[List[int]]
+        Generates the first n rows of Pascal's Triangle.
+"""
+
+from typing import List
+
+
+def factorial(num: int) -> int:
+    """
+    Computes the factorial of a number.
+
+    Args:
+        num (int): The number to compute the factorial of.
+
+    Returns:
+        int: The factorial of the given number.
+    """
     if num == 0:
         return 1
     else:
         return num * factorial(num - 1)
 
 
-def combination(n, r):
+def combination(n: int, r: int) -> int:
+    """
+    Computes the number of combinations (n choose r).
+
+    Args:
+        n (int): The total number of items.
+        r (int): The number of items to choose.
+
+    Returns:
+        int: The number of combinations.
+    """
     return factorial(n) // (factorial(r) * factorial(n - r))
 
 
-def pascal_triangle(n) -> int:
+def pascal_triangle(n: int) -> List[List[int]]:
+    """
+    Generates the first n rows of Pascal's Triangle.
+
+    Args:
+        n (int): The number of rows to generate.
+
+    Returns:
+        List[List[int]]: A list of lists representing Pascal's Triangle.
+    """
     triangle = []
     for numbers in range(n):
         current_row = []
