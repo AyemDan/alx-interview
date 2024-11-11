59bb0c84a9e5e8b18a4e584b9987c204a3224b76
AyemDanm <danielMMomodu@gmail.com>
2024-11-11 23:07:08 +0100

    Starwars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,29 +2,39 @@
 
 const request = require('request');
 
-async function getUsers() {
-    try {
-        const argValue = process.argv[2];
-        const intValue = parseInt(argValue, 10);
+async function getUsers () {
+  try {
+    const argValue = process.argv[2];
+    const intValue = parseInt(argValue, 10);
 
-        if (isNaN(intValue)) {
-            console.error('Please provide a valid integer as the argument');
-            return;
-        }
-        
-        request(`https://swapi-api.alx-tools.com/api/films/${intValue}/`, function (error, response, body) {
-            const data = JSON.parse(body);
-            const characters = data.characters;
-
-            for (let i = 0; i < characters.length; i++) {
-                const character = request(characters[i], function (error, response, body) {
-                    const data = JSON.parse(body);
-                    console.log(data.name);
-                })
-            }  
-});
-    } catch (error) {
-        console.error("Error fetching data:", error);
+    if (isNaN(intValue)) {
+      console.error('Please provide a valid integer as the argument');
+      return;
     }
+
+    request(`https://swapi-api.alx-tools.com/api/films/${intValue}/`, function (error, response, body) {
+      if (error) {
+        console.error('Error with film request:', error);
+        return;
+      }
+
+      const data = JSON.parse(body);
+      const characters = data.characters;
+
+      for (let i = 0; i < characters.length; i++) {
+        request(characters[i], function (error, response, body) {
+          if (error) {
+            console.error('Error with character request:', error);
+            return;
+          }
+
+          const data = JSON.parse(body);
+          console.log(data.name);
+        });
+      }
+    });
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
 }
 getUsers();
