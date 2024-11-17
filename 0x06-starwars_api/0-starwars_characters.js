1188ee5b04fbbb7e0659f5274ad200f5e0dd1116
AyemDanm <danielMMomodu@gmail.com>
2024-11-17 19:09:31 +0100

    Changes

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,51 +1,25 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-// Get the Movie ID from the command-line arguments
 const movieId = process.argv[2];
+const options = {
+  url: 'https://swapi-api.hbtn.io/api/films/' + movieId,
+  method: 'GET'
+};
 
-if (!movieId) {
-  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
-  process.exit(1);
-}
-
-// Star Wars API URL for the specified Movie ID
-const filmUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-
-// Fetch the film data
-request(filmUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error fetching film data:', error);
-    return;
-  }
-
-  if (response.statusCode !== 200) {
-    console.error('Error: Invalid Movie ID or API issue');
-    return;
-  }
-
-  try {
-    const filmData = JSON.parse(body);
-    const characterUrls = filmData.characters;
-
-    // Fetch and display each character in the same order as the API
-    characterUrls.forEach((url) => {
-      request(url, (error, response, body) => {
-        if (error) {
-          console.error('Error fetching character data:', error);
-          return;
-        }
-
-        try {
-          const characterData = JSON.parse(body);
-          console.log(characterData.name);
-        } catch (parseError) {
-          console.error('Error parsing character data:', parseError);
-        }
-      });
-    });
-  } catch (parseError) {
-    console.error('Error parsing film data:', parseError);
+request(options, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
